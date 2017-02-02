66c31ca57c183fd260215d07718e10ac32a5e50a
Francely Peralta <Francely@Francelys-MBP.home>
2017-02-02 14:20:46 -0500

    set up collectionview

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Francely peralta on 2/2/17.
+//  Copyright © 2017 Francely peralta. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+}
